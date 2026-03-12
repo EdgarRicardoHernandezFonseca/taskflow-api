03ee495a4ee171393798ed3a775e0a462a1b30c1
Edgar Ricardo Hernandez Fonseca <erhernandez2000@yahoo.com>
2026-03-12 15:24:38 -0500

    test: implement unit tests for UserServiceImpl using Mockito

---
+++ src/test/java/com/edgar/taskflow/service/impl/UserServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.edgar.taskflow.service.impl;
+
+import com.edgar.taskflow.dto.UserRequestDTO;
+import com.edgar.taskflow.dto.UserResponseDTO;
+import com.edgar.taskflow.entity.Role;
+import com.edgar.taskflow.entity.User;
+import com.edgar.taskflow.repository.UserRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    private UserRequestDTO request;
+
+    @BeforeEach
+    void setUp() {
+
+        request = new UserRequestDTO();
+        request.setUsername("edgar");
+        request.setEmail("edgar@email.com");
+        request.setPassword("123456");
+    }
+
+    @Test
+    void shouldCreateUser() {
+
+        User savedUser = User.builder()
+                .id(1L)
+                .username("edgar")
+                .email("edgar@email.com")
+                .password("123456")
+                .role(Role.USER)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Mockito.when(userRepository.save(Mockito.any(User.class)))
+                .thenReturn(savedUser);
+
+        UserResponseDTO response = userService.createUser(request);
+
+        assertNotNull(response);
+        assertEquals("edgar", response.getUsername());
+        assertEquals("edgar@email.com", response.getEmail());
+
+        Mockito.verify(userRepository).save(Mockito.any(User.class));
+    }
+}
